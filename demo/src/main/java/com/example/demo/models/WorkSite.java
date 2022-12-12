3945790794f5309dd67e8daf9b87be3495e44a5d
LAPTOP-N8E0I7QK\gotou <gotoukei57@gmail.com>
2022-12-12 21:03:50 +0900

    first commit

---
+++ demo/src/main/java/com/example/demo/models/WorkSite.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+// TODO: Entityとかアノテーションの効果
+@Entity
+// TODO: hibernateのtableとpresistenceのtableの違い
+@Table(name = "worksites")
+public class WorkSite {
+
+}
